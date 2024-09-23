8afe238521973ca369daa257f90d4b890243b28f
João Bertholino <joaobertholino964@gmail.com>
2024-09-23 13:57:35 -0300

    init: Initial template Spring project.

---
+++ src/main/java/dev/joaobertholino/dockerdiochallenge/DockerDioChallengeApplication.java
@@ -0,0 +1,13 @@
+package dev.joaobertholino.dockerdiochallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerDioChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerDioChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/joaobertholino/dockerdiochallenge/DockerDioChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.joaobertholino.dockerdiochallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerDioChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
