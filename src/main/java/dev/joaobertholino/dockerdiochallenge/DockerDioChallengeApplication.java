b4499bed949c9a51f01463d749f55c0b40932e07
João Bertholino <joaobertholino964@gmail.com>
2024-09-26 17:33:46 -0300

    feat: Driver class

--- src/main/java/dev/joaobertholino/dockerdiochallenge/DockerDioChallengeApplication.java
+++ src/main/java/dev/joaobertholino/dockerdiochallenge/DockerDioChallengeApplication.java
@@ -2,6 +2,7 @@ package dev.joaobertholino.dockerdiochallenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 public class DockerDioChallengeApplication {
